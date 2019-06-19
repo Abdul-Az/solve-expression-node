8b79dd4935d658f442010a6c53d76d9f5336c3af
Abdul Azeez <abdaseez@gmail.com>
2019-06-19 21:00:01 +0530

    x found

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//Import json data
 const input = require('./input.json')
 
 //Assing all values to  variables
@@ -29,5 +30,33 @@ const operator = (op) => {
     console.log('wrong operator')
 }
 //Print Step 1
-console.log(  `(${lhs_rhs_lhs} ${lhs_rhs_rhs}) ${rhs} ${lhs_lhs} ${operator(lhs_op)} ${operator(lhs_rhs_op)} ${operator(equal_op)}`  )
+console.log(  `(${lhs_rhs_lhs} ${lhs_rhs_lhs}) ${rhs} ${lhs_lhs} ${operator(lhs_op)} ${operator(lhs_rhs_op)} ${operator(equal_op)}`  )
 
+//Step 2
+let other_numbers = [ lhs_rhs_lhs, lhs_rhs_lhs, lhs_lhs]
+
+//find x
+let x = []
+const find_x_in_other_numbers = (arr_of_other_numbers) => {
+    for(i=0; i < arr_of_other_numbers.length; i++ ){
+        if(arr_of_other_numbers[2] === 'x'){
+            x.push('-x')
+            arr_of_other_numbers.pop()
+            return arr_of_other_numbers
+        }
+       if(arr_of_other_numbers[0] === 'x'){
+           x.push('1/x')
+           arr_of_other_numbers.shift()
+           return arr_of_other_numbers
+       } 
+       if(arr_of_other_numbers[1] === 'x'){
+           x.push('1/x')
+           arr_of_other_numbers.splice(1)
+           return arr_of_other_numbers
+       }
+    }
+ 
+}
+
+find_x_in_other_numbers(other_numbers)
+console.log(x)
